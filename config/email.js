dd63179d642442a98d94aba86129c9325f527feb
saadullahkhan12 <muhammadsaadullah093@gmail.com>
2025-09-14 03:37:17 +0500

    saad

--- config/email.js
+++ config/email.js
@@ -8,14 +8,6 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-// Alternative for other services (SendGrid, Mailgun, etc.)
-// const transporter = nodemailer.createTransport({
-//   host: process.env.SMTP_HOST,
-//   port: process.env.SMTP_PORT,
-//   auth: {
-//     user: process.env.SMTP_USER,
-//     pass: process.env.SMTP_PASS
-//   }
-// });
+
 
 module.exports = transporter;
